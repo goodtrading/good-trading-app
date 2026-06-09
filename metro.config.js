1377a9ad44c8d3f2b7dc9d8faac1d54aae38b6fb
goodtrading <goodtradingpay@gmail.com>
2026-06-09 00:24:00 -0300

    fix metro config compatibility for expo web build

--- metro.config.js
+++ metro.config.js
@@ -1,3 +1,10 @@
+// Polyfill for Node 18 compatibility
+if (!Array.prototype.toReversed) {
+  Array.prototype.toReversed = function() {
+    return [...this].reverse();
+  };
+}
+
 const { getDefaultConfig } = require("expo/metro-config");
 
 module.exports = getDefaultConfig(__dirname);
